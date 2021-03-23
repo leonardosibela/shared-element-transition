d0557af4944ffe6599056b1f0c222a0a6f3ed82d
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-23 10:41:43 -0300

    Adding fragment classes

---
+++ app/src/main/java/com/sibela/sharedelementtransition/ImageFragment.kt
@@ -0,0 +1,9 @@
+package com.sibela.sharedelementtransition
+
+import androidx.fragment.app.Fragment
+
+class ImageFragment : Fragment(R.layout.fragment_image) {
+
+
+
+}
---
+++ app/src/main/java/com/sibela/sharedelementtransition/MainFragment.kt
@@ -0,0 +1,9 @@
+package com.sibela.sharedelementtransition
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+
+
+}
