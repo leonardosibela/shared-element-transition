00ca3716166dd7d111e806061580a994d34c3d2b
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-23 10:56:22 -0300

    Implementing transition

--- app/src/main/java/com/sibela/sharedelementtransition/ImageFragment.kt
+++ app/src/main/java/com/sibela/sharedelementtransition/ImageFragment.kt
@@ -1,9 +1,18 @@
 package com.sibela.sharedelementtransition
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.transition.TransitionInflater
 
 class ImageFragment : Fragment(R.layout.fragment_image) {
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val animation = TransitionInflater.from(requireContext()).inflateTransition(
+            android.R.transition.move
+        )
+        sharedElementEnterTransition = animation
+        sharedElementReturnTransition = animation
+    }
 
 }
--- app/src/main/java/com/sibela/sharedelementtransition/MainFragment.kt
+++ app/src/main/java/com/sibela/sharedelementtransition/MainFragment.kt
@@ -1,9 +1,30 @@
 package com.sibela.sharedelementtransition
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import com.sibela.sharedelementtransition.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding get() = _binding!!
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentMainBinding.bind(view)
+
+        binding.image.setOnClickListener {
+            val extras = FragmentNavigatorExtras(binding.image to "image_big")
+            findNavController().navigate(
+                R.id.action_mainFragment_to_imageFragment,
+                null,
+                null,
+                extras
+            )
+        }
+    }
 
 }
